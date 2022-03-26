415959146b1a9942d82c043813aad90c47a009f1
Andrea Lai <laiandrea2@gmail.com>
2022-03-26 16:44:47 +0100

    aligned github

--- api/index.js
+++ api/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(5000, () => {
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+// Dotenv congig required
+dotenv.config();
+
+// DB Connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Allow app to use JSON
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
