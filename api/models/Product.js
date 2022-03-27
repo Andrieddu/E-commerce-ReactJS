f25fabfbe8d181e6fb67dfd2e4af4fd204b093fa
Andrea Lai <laiandrea2@gmail.com>
2022-03-27 20:21:20 +0200

    completed api

--- api/models/Product.js
+++ api/models/Product.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true },
+    description: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array },
-    size: { type: Array },
-    color: { type: Array },
+    size: { type: String }, // Might be an array
+    color: { type: String }, // Might be an array
     price: { type: Number, required: true },
   },
   { timestamps: true }
